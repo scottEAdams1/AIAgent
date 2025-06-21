d7698ef6fd59f7c309a7449cffa678051508fc0d
scottEAdams1 <scott.e.adams01@gmail.com>
2025-06-21 17:53:30 +0100

    Agent can now loop

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from config import system_prompt
+from config import system_prompt, MAX_ITER
 from call_function import available_functions, call_function
 
 load_dotenv()
@@ -21,23 +21,29 @@ client = genai.Client(api_key=api_key)
 
 prompt = " ".join(args)
 messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt, tools=[available_functions]))
+for i in range(MAX_ITER):
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt, tools=[available_functions]))
 
-if not response.function_calls:
-    print(response.text)
-    
-else:
-    for function_call_part in response.function_calls:
-        result = call_function(function_call_part, verbose)
-        try:
-            response_data = result.parts[0].function_response.response
-        except Exception as e:
-            print("Error: function did not return response")
-            sys.exit(1)
-        if verbose:
-            print(f"-> {response_data}")
+    if not response.function_calls:
+        print(response.text)
+        break
+        
+    else:
+        for candidate in response.candidates:
+            messages.append(candidate.content)
+        for function_call_part in response.function_calls:
+            result = call_function(function_call_part, verbose)
+            messages.append(result)
+            try:
+                response_data = result.parts[0].function_response.response
+            except Exception as e:
+                print("Error: function did not return response")
+                sys.exit(1)
+            if verbose:
+                print(f"-> {response_data}")
 
-if verbose:
-    print(f"User prompt: {prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if verbose:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
